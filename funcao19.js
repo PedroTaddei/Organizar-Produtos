593136fed86746f14d433b3e42f40d7aa0834dd2
Pedro Neves <pedro.t.dasneves@gmail.com>
2022-11-01 20:04:42 -0300

    Buscar pelo nome

--- funcao19.js
+++ funcao19.js
@@ -12,10 +12,18 @@ function CadastrarProdutos() {
      index++
 }
 
-function BuscarPorId(){
+function BuscarPorId() {
      for (var contador = 0; contador < ids.length; contador++) {
-          if(id == ids[contador]) {
+          if (id == ids[contador]) {
                console.log("O produto de ID ", id, " é o ", nomes[contador], " custa ", precos[contador], " e tem avaliação ", avaliacoes[contador])
           }
-     }         
+     }
+}
+
+function BuscarPorNome(nome) {
+     for (var contador = 0; contador < nome.length; contador++) {
+          if (nome == nomes[contador]) {
+               console.log("O produto de nome ", nome, " tem o id", ids[contador])
+          }
+     }
 }
