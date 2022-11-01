ee7475c6eed8750fc15cbda168165bef2e164e53
Pedro Neves <pedro.t.dasneves@gmail.com>
2022-11-01 20:00:25 -0300

    Buscar ID

--- funcao19.js
+++ funcao19.js
@@ -1,86 +1,21 @@
+var ids = []
 var nomes = []
-var senhas = []
+var precos []
+var avaliacoes = []
 var index = 0
-var arraySupNomes = []
-var arraySupSenhas = []
-var indexSup = 0
-var continuar = true
 
-function CadastrarUsuario() {
-     nomes[index] = prompt("Insira o nome para cadastro")
-     senhas[index] = prompt("Crie sua senha para cadastro")
+function CadastrarProdutos() {
+     ids[index] = parseInt(prompt("Insira o ID do produto"))
+     nome[index] = prompt("Insira o nome do produto")
+     precos[index] = prompt("Insira o preço do produto")
+     avaliacoes[index] = prompt("Insira a avaliação do produto")
      index++
 }
 
-function EscolherOpcao() {
-     var opcao = parseInt(prompt("Insira a função de cadastro desejada. 1 = cadastro; 2 = login; 3 = excluir cadastro; 4 = encerrar"))
-     return opcao
-}
-
-function FazerLogin(nome, senha) {
-     for (var contador = 0; contador < nomes.length; contador++) {
-          if (nome == nome[contador]) {
-               if (senha == senhas[contador]) {
-                    return true
-               }
+function BuscarPorId(){
+     for (var contador = 0; contador < ids.length; contador++) {
+          if(id == ids[contador]) {
+               console.log("O produto de ID ", id, " é o ", nomes[contador], " custa ", precos[contador], " e tem avaliação ", avaliacoes[contador])
           }
-     }
-     return false
-
-}
-
-function ExcluirCadastro(nome) {
-     for (let contador = 0; contador < nomes.length; contador++) {
-          if (nome == nomes[contador]) {
-               nomes[contador] = 0
-               senhas[contador] = 0
-          }
-     }
-     for (var contador = 0; contador < nomes.length; contador++) {
-          if (nomes[contador] != 0) {
-               arraySupNomes[indexSup] = nomes[contador]
-               arraySupSenhas[indexSup] = senhas[contador]
-               indexSup++
-          }
-     }
-     nomes = arraySupNomes
-     senhas = arraySupSenhas
-}
-
-while (continuar) {
-     var opcao = EscolherOpcao()
-
-     if (opcao == 1) {
-          CadastrarUsuario()
-          console.log("Usuario Cadastrado com sucesso!")
-     } else if (opcao == 2) {
-          var nome = prompt("Insira um nome para login")
-          var senha = prompt("Insira uma senha para login")
-          var login = FazerLogin(nome, senha)
-          if (login == true) {
-               console.log("Login feito com sucesso!")
-          } else {
-               console.log("Nome ou senha incorretos!")
-          }
-     } else if (opcao == 3) {
-
-          var nome = prompt("Insira um nome de cadastro que deseja excluir")
-          ExcluirCadastro(nome)
-          console.log("Cadastro excluido com sucesso")
-
-     } else {
-
-          console.log("O programa será encerrado")
-          continuar = false
-     }
-
-     if (continuar = true) {
-          var opcaoContinuar = prompt("Deseja continuar com as operações? s ou n?")
-          if (opcaoContinuar == "s") {
-               continuar = true
-          }  else {
-               continuar = false
-          }
-     }
-
+     }         
 }
