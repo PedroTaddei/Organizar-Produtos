6ea2bbfc0f20e6f99f6a29bfc48ab4a1964b3e2d
Pedro Neves <pedro.t.dasneves@gmail.com>
2022-11-01 20:21:30 -0300

    Organizar por ID

--- funcao19.js
+++ funcao19.js
@@ -1,8 +1,13 @@
 var ids = []
 var nomes = []
-var precos []
+var precos = []
 var avaliacoes = []
 var index = 0
+var idsSup = []
+var nomesSup = []
+var precosSup = []
+var avaliacoesSup = []
+var indexSup = 0
 
 function CadastrarProdutos() {
      ids[index] = parseInt(prompt("Insira o ID do produto"))
@@ -24,6 +29,25 @@ function BuscarPorNome(nome) {
      for (var contador = 0; contador < nome.length; contador++) {
           if (nome == nomes[contador]) {
                console.log("O produto de nome ", nome, " tem o id", ids[contador])
+          }
+     }
+}
+
+function ArrumarPorId() {
+     for (var contador = 0; contador < ids.length; contador++) {
+          maiorId = ids[contador]
+          for (var contador2 = 0; contador2 < ids.length; contador2++) {
+               if (ids[contador] > maiorId) {
+                    maiorId = ids[contador2]
+               }
+          }
+          for (var contador3 = 0; contador3 < ids.length; contador3++) {
+               if (maiorId == ids[contador3]) {
+                    idsSup[indexSup] = ids[contador3]
+                    nomesSup[indexSup] = nomes[contador3]
+                    precosSup[indexSup] = precos[contador3]
+                    avaliacoesSup[indexSup] = avaliacoes[contador3]
+               }
           }
      }
 }
